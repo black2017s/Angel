727f75a7b0c3cf4a90f5045a7b9e2a49520eee0a
Ahmed Sayed <ahmed1265sa@gmail.com>
2024-10-26 10:04:46 +0300

    u

--- script.js
+++ script.js
@@ -20,19 +20,19 @@ function updateTimer() {
 // تحديث كل ثانية
 setInterval(updateTimer, 1000);
 
-const apiKey = '3a503f48dcc848eb940962666554070e'; // أدخل مفتاح API هنا
-        fetch(`https://api.ipgeolocation.io/ipgeo?apiKey=${apiKey}`)
-            .then(response => {
-                if (!response.ok) {
-                    throw new Error('Network response was not ok');
-                }
-                return response.json();
-            })
-            .then(data => {
-                const location = `${data.city}, ${data.state_prov}, ${data.country_name}`; // تجميع المدينة، المحافظة، والدولة
-                document.getElementById('location').textContent = location; // عرض الموقع
-            })
-            .catch(error => {
-                console.error('Error fetching the location:', error);
-                document.getElementById('location').textContent = 'Unable to fetch location'; // رسالة في حالة الخطأ
-            });
+const apiKey = '2359385f1106449aa7ade1b57167b4fb'; // أدخل مفتاح API هنا
+fetch(`https://api.ipgeolocation.io/ipgeo?apiKey=${apiKey}`)
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        return response.json();
+    })
+    .then(data => {
+        const location = `${data.city}, ${data.state_prov}, ${data.country_name}`; // تجميع المدينة، المحافظة، والدولة
+        document.getElementById('location').textContent = location; // عرض الموقع
+    })
+    .catch(error => {
+        console.error('Error fetching the location:', error);
+        document.getElementById('location').textContent = 'Unable to fetch location'; // رسالة في حالة الخطأ
+    });
